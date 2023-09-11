b45cd91d932e80827209b09b6a7a5c2c184f08dc
waltereidi <walter-eidi@hotmail.com>
2023-09-10 21:06:59 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script>
+import Button from './components/button.vue';
+export default {
+  components: {
+    Button
+  }
+}
+</script>
+
+<template>
+  <div class="headerContainer">
+    <div class="header">
+      <div class="menu">
+        <p>Item1</p>
+      </div>
+      <div class="menu">
+        <p>Item2</p>
+      </div>
+      <div class="menu">
+        <p>Item3</p>
+      </div>
+      <div class="menu-last">
+        <p>Item4</p>
+      </div>
+
+    </div>
+  </div>
+  <div class="content">
+    <div class="leftBar">
+      <a href="#" class="menuLeft">Item1</a>
+      <a href="#" class="menuLeft">Item2</a>
+      <a href="#" class="menuLeft">Item3</a>
+      <a href="#" class="menuLeft">Item4</a>
+    </div>
+    <div class="mainContent">
+      <Button>
+        <template v-slot:botao1>
+          nome
+        </template>
+        <template v-slot:botao2>
+          sobrenome
+        </template>
+
+      </Button>
+
+    </div>
+    <div class="rightBar">
+
+    </div>
+  </div>
+  <div class="footer"></div>
+</template>
+
+<style scoped>
+@import "@/assets/main.scss";
+</style>
---
+++ src/components/button.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+
+}
+
+</script>
+<template>
+    <button>
+        <slot name="botao1"></slot>
+        <slot name="botao2"></slot>
+    </button>
+</template>
